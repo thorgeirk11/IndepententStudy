e674b5891dabce3edb746e4fa161c9ed6d7d4dd2
Þorgeir <thorgeir@radiantgames.is>
2017-06-01 18:41:34 +0000

    Setup the folder structure better and hand a meeting with stephan

---
+++ ReadMe.md
@@ -0,0 +1,20 @@
+# Connect4 
+
+## Extracting data:
+The sql scripts for extracting moves and states are located under the sql folder:
+### Parse.sql 
+    This 
+
+1. Do not aggegate the output of training.
+    - Maby just copy paste..
+
+2. Split up the output of diffrent roles.. 
+   - have 14 actions for connect4
+
+3. Once you get somthing working split up the output layer for diffrent roles.
+
+4. Use somthing like SSD sum of sqare diffrentances 
+    Look at alpha go! :)  
+    - Sum ( o(x) - y ) ^ 2
+
+ 
--- connect4.py
+++ connect4.py
@@ -8,39 +8,83 @@ import numpy as np
 import time
 from datetime import timedelta
 import math
+from collections import defaultdict 
 import numpy
 
 # We also need PrettyTensor.
 import prettytensor as pt
 import pandas as pd
+import random
 
 state_size = 135
 num_actions = 7
 
-df=pd.read_csv('StatesAndActions.csv', usecols = range(0,state_size), header = None)
-d = df.values
-l = pd.read_csv('StatesAndActions.csv', usecols = range(state_size, state_size + num_actions), header = None)
-labels = l.values
-print(data)
-print(labels)
-
-
-
-
-x = tf.placeholder(tf.bool, shape=(None, state_size), name='x')
-y_true = tf.placeholder(tf.bool, shape=[None, num_actions], name='y_true')
+x = tf.placeholder(tf.float32, shape=[None, state_size], name='x')
+y_true = tf.placeholder(tf.float32, shape=[None, num_actions], name='y_true')
 y_true_cls = tf.argmax(y_true, dimension=1)
 
 x_pretty = pt.wrap(x)
 with pt.defaults_scope(activation_fn=tf.nn.relu):
     y_pred, loss = x_pretty.\
-        fully_connected(size=128, name='layer_fc1').\
-        softmax_classifier(num_classes=num_classes, labels=num_actions)
+        fully_connected(size=state_size*2, name='layer_fc1').\
+        fully_connected(size=state_size, name='layer_fc2').\
+        fully_connected(size=50, name='layer_fc3').\
+        fully_connected(size=state_size, name='layer_fc4').\
+        fully_connected(size=num_actions, name='layer_fc5').\
+        softmax_classifier(num_classes=num_actions, labels=y_true)
 
-optimizer = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(loss)
+optimizer = tf.train.AdamOptimizer(learning_rate=1e-5).minimize(loss)
+
 y_pred_cls = tf.argmax(y_pred, dimension=1)
+
 correct_prediction = tf.equal(y_pred_cls, y_true_cls)
+
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+def read_data(file_name):
+    features = pd.read_csv(file_name, usecols = range(0,state_size), header = None)
+    labels  = pd.read_csv(file_name, usecols = range(state_size, state_size + num_actions), header = None)
+    data = zip(features.values, labels.values)
+    data_dict = defaultdict(list)
+    for v, k in data: data_dict[tuple(v)].append(k)
+
+def 
+    weighted_data = []
+    for key, labels in data_dict.items():
+        weighed_labels = [0, 0, 0, 0, 0, 0, 0]
+        for label in labels:
+            i = 0
+            for v in label:
+                if (v > 0):
+                    weighed_labels[i] += 1
+                    break
+                i += 1
+        count = len(labels)
+        for i in range(0,len(weighed_labels)):
+            weighed_labels[i] /= count
+        weighted_data.append( (list(key),weighed_labels) )
+
+    return weighted_data
+
+train_data, test_data = read_data('Connect4_data_role1.csv.csv')
+
+
+# save to file:
+with open('Connect4_train_processed.json', 'w') as f:
+    for k, v in train_data:
+        line = '{}, {}'.format(k, v) 
+        print(line, file=f)     
+
+print(len(train_data))
+print(len(test_data))
+def read_from_csv(batch_size):
+    features = []
+    labels = []
+    for i in range(0, batch_size):
+        feature, label = random.choice(train_data)
+        features.append(feature)
+        labels.append(label)
+    return features, labels
 
 
 session = tf.Session()
@@ -63,7 +107,7 @@ def optimize(num_iterations):
         # Get a batch of training examples.
         # x_batch now holds a batch of images and
         # y_true_batch are the true labels for those images.
-        x_batch, y_true_batch = data.train.next_batch(train_batch_size)
+        x_batch, y_true_batch = read_from_csv(train_batch_size)
 
         # Put the batch into a dict with the proper names
         # for placeholder variables in the TensorFlow graph.
@@ -78,13 +122,28 @@ def optimize(num_iterations):
         # Print status every 100 iterations.
         if i % 100 == 0:
             # Calculate the accuracy on the training-set.
-            acc = session.run(accuracy, feed_dict=feed_dict_train)
+            acc_train = session.run(accuracy, feed_dict=feed_dict_train)
+            
+            feed_dict = {x: [i[0] for i in test_data] ,
+                            y_true: [i[1] for i in test_data] }
+
+            acc = session.run(accuracy, feed_dict=feed_dict)
+            
+            example = test_data[15]
+            feed_dict_2 = {x: [example[0]],
+                            y_true: [example[1]] }
+            predictied = session.run(y_pred, feed_dict= feed_dict_2)
+            l = session.run(loss, feed_dict= feed_dict_2)
+            print(list(predictied[0]))
+            print(example[1])
+            print(l)
+
 
             # Message for printing.
-            msg = "Optimization Iteration: {0:>6}, Training Accuracy: {1:>6.1%}"
-
+            msg = "Optimization Iteration: {0:>6}, Train Accuracy: {1:>6.1%}, Test Accuracy: {2:>6.1%}"
+            
             # Print it.
-            print(msg.format(i + 1, acc))
+            print(msg.format(i + 1, acc_train, acc))
 
     # Update the total number of iterations performed.
     total_iterations += num_iterations
@@ -98,55 +157,4 @@ def optimize(num_iterations):
     # Print the time-usage.
     print("Time usage: " + str(timedelta(seconds=int(round(time_dif)))))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# print(value)
-# print(key)
-# 
-# record_defaults = [ 
-#     [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1],
-#     [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1],
-#     [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1],
-#     [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1],
-#     [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1],
-#     [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1],
-#     [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1],
-#     [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1],
-#     [1], [1], [1], [1], [1], [1], [1], "(Drop 1)"
-# ]
-
-
-    # with open('StatesAndActions.csv', newline='') as csvfile:
-    #     spamreader = csv.reader(csvfile, delimiter=',')
-    #     for row in spamreader:
-    #         print(', '.join(row))
-    # return 1,2,3
-    
-    
+optimize(3000)

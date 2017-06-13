89202961b2efbe12bb2032116cb17101b8373605
Þorgeir <thorgeir@radiantgames.is>
2017-06-13 13:29:53 +0000

    Cleaned up the connect4Ploter

--- ChineseCheckers/cc6Ploter.py
+++ ChineseCheckers/cc6Ploter.py
@@ -20,13 +20,11 @@ metadata_size = 4
 state_size = 253
 num_actions = 90
 
-
 # -------------------------------------------------------------------
 #                           Read Data
 # -------------------------------------------------------------------
 
 def read_data(file_name, train_data_size, fun):
-    # Match.connect4.1240881643605, 1, 1, 100
     meta = pd.read_csv(file_name, usecols = range(0,metadata_size), header = None)
     features = pd.read_csv(file_name, usecols = range(metadata_size,metadata_size+state_size), header = None)
     labels  = pd.read_csv(file_name, usecols = range(metadata_size + state_size, metadata_size + state_size + num_actions), header = None)
@@ -38,67 +36,20 @@ def read_data(file_name, train_data_size, fun):
     train_data = data[:cut]
     test_data =  data[cut:]
     return train_data, test_data
-
-def process_probatility(data):
-    data_dict = defaultdict(list)
-    for v, k in data: data_dict[tuple(v)].append(k)
-    weighted_data = {}
-    for key, labels in data_dict.items():
-        weighed_labels = [0 for i in range(0, num_actions)]
-        for label in labels:
-            i = 0
-            for v in label:
-                if (v > 0):
-                    weighed_labels[i] += 1
-                    break
-                i += 1
-        count = len(labels)
-        for i in range(0,len(weighed_labels)):
-            weighed_labels[i] /= count
-        weighted_data[tuple(key)] = weighed_labels
-    return weighted_data
   
 def filterData(record):
     meta, _, _ = record
     return meta[2] == 0 and meta[3] > 0
 
-def guess_dumbly(data):
-    correct = [0,0,0,0,0,0,0,0]
-    for v, k in data:
-        for i in range(0, len(k)):
-            if (k[i] == 1):
-                correct[i] += 1
-    print(correct)
-    print(len(data))
-
-lastIndex = 0
-def read_from_csv(batch_size, rand):
-    global lastIndex
+def read_from_csv(batch_size):
     features = []
     labels = []
-    if rand:
-        for i in range(0, batch_size):
-            feature, label = random.choice(train_data)
-            features.append(feature)
-            labels.append(label)
-        return features, labels
 
-    trainLen = len(train_data)
-    if lastIndex + batch_size < trainLen:
-        end = lastIndex + batch_size
-        batch = train_data[lastIndex:end]
-        lastIndex = end
-    else:
-        rest = lastIndex + batch_size - trainLen
-        batch = train_data[lastIndex:] + train_data[:rest]
-        lastIndex = rest         
-        
-    for feature, label in batch:
+    for i in range(0, batch_size):
+        feature, label = random.choice(train_data)
         features.append(feature)
         labels.append(label)
-
     return features, labels
-
 
 # -------------------------------------------------------------------
 #                           Create Model
@@ -108,37 +59,43 @@ x = tf.placeholder(tf.float32, shape=[None, state_size], name='x')
 y_true = tf.placeholder(tf.float32, shape=[None, num_actions], name='y_true')
 y_true_cls = tf.argmax(y_true, dimension=1)
 
-def fully_connected_network():#layers, randSample):
-    #x_pretty =
-    #with pt.defaults_scope(activation_fn=tf.nn.relu):
-        #network = x_pretty
-        #i = 1
-        #for size in layers:
-        #    print(size)
-        #    network.fully_connected(size=size, name='layer_fc{0}'.format(i))
-        #    i += 1
-    y_pred, loss =  pt.wrap(x).softmax(num_classes=num_actions,labels=y_true)
-    #if randSample:
-    #    loss = tf.reduce_mean(tf.squared_difference(y_pred, y_true))
+def fully_connected_network(layers):
+    x_pretty = pt.wrap(x)
+    with pt.defaults_scope(activation_fn=tf.nn.relu):
+        network = x_pretty
+        i = 1
+        for size in layers:
+            #with tf.name_scope('fully_connected'):
+            network = network.fully_connected(size=size, name='layer_fc{0}_{1}'.format(i, size))
+            i += 1
+        with tf.name_scope("softmax"):
+            y_pred, _ = network.softmax_classifier(num_classes=num_actions, labels=y_true)
+            loss = tf.reduce_mean(tf.squared_difference(y_pred, y_true))
 
-    y_pred_cls = tf.argmax(y_pred, dimension=1)
-    correct_prediction = tf.equal(y_pred_cls, y_true_cls)
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    optimizer = tf.train.AdamOptimizer().minimize(loss)
-    name = ', '.join(str(x) for x in layers) + (' Default_Loss' if randSample else '') 
-    return ('WAT', optimizer, accuracy, ([],[]), randSample)
+        with tf.name_scope("accuracy"):
+            y_pred_cls = tf.argmax(y_pred, dimension=1)
+            correct_prediction = tf.equal(y_pred_cls, y_true_cls)
+            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        
+        with tf.name_scope("train"):
+            optimizer = tf.train.AdamOptimizer(learning_rate=1e-5).minimize(loss)
+
+        name = ', '.join(str(x) for x in layers) 
+        return (name, optimizer, accuracy, ([],[]), (y_pred,loss))
+
 
 networks = [ 
-    fully_connected_network()#[], True),
+    fully_connected_network([]),
 ]
 
 
     
 # Splits the training and test data 80/20.
 train_data, test_data = read_data('data/chinese_checkers_6_role0_noNoop.csv', 0.8, filterData)
-#probatilities = process_probatility(train_data + test_data)
-#guess_dumbly(train_data+test_data)
 
+# -------------------------------------------------------------------
+#                           Train Model
+# -------------------------------------------------------------------
 
 session = tf.Session()
 session.run(tf.global_variables_initializer())
@@ -152,18 +109,12 @@ def optimize(num_iterations):
     feed_dict_test = {x: [i[0] for i in test_data] ,
                       y_true: [i[1] for i in test_data] }
     for i in range(0, num_iterations):
-        x_batch_rnd, y_true_batch_rnd = read_from_csv(train_batch_size,True)
-        #x_batch, y_true_batch = read_from_csv(train_batch_size,False)
+        x_batch, y_true_batch = read_from_csv(train_batch_size)
 
-        feed_dict_train_rnd = {x: x_batch_rnd,
-                            y_true: y_true_batch_rnd}
-        #feed_dict_train = {x: x_batch,
-        #                    y_true: y_true_batch}
+        feed_dict_train = {x: x_batch,
+                            y_true: y_true_batch}
         for _, optimizer, _, _, rndSample in networks:
-            #if rndSample:
-                session.run(optimizer, feed_dict=feed_dict_train_rnd)
-            #else:
-            #    session.run(optimizer, feed_dict=feed_dict_train)
+            session.run(optimizer, feed_dict=feed_dict_train_rnd)
 
         if i % 10 == 0:
             for name, _, accuracy, (plotx,ploty), _ in networks:
@@ -179,14 +130,8 @@ def optimize(num_iterations):
     time_dif = end_time - start_time
     print("Time usage: " + str(timedelta(seconds=int(round(time_dif)))))
 
-def argmax_label(label):
-    maxi = 0
-    for i in range(0, len(label)):
-        if (label[i] > label[maxi]):
-            maxi = i
-    return maxi
-
 optimize(2000)
+
 for (name, (x,y)) in [(i[0], i[3]) for i in networks]:
     plt.plot(x,y,label=name)
 plt.ylabel('Test Accuracy (%)')
--- Connect4/connect4Ploter.py
+++ Connect4/connect4Ploter.py
@@ -16,19 +16,18 @@ import prettytensor as pt
 import pandas as pd
 import random
 
+metadata_size = 4
 state_size = 135
 num_actions = 8
-
 
 # -------------------------------------------------------------------
 #                           Read Data
 # -------------------------------------------------------------------
 
 def read_data(file_name, train_data_size, fun):
-    # Match.connect4.1240881643605, 1, 1, 100
-    meta = pd.read_csv(file_name, usecols = range(0,4), header = None)
-    features = pd.read_csv(file_name, usecols = range(4,4+state_size), header = None)
-    labels  = pd.read_csv(file_name, usecols = range(4+state_size, 4 + state_size + num_actions), header = None)
+    meta = pd.read_csv(file_name, usecols = range(0,metadata_size), header = None)
+    features = pd.read_csv(file_name, usecols = range(metadata_size,metadata_size+state_size), header = None)
+    labels  = pd.read_csv(file_name, usecols = range(metadata_size + state_size, metadata_size + state_size + num_actions), header = None)
     
     data = list(zip(meta.values, features.values, labels.values))
     data = [(item[1],item[2]) for item in data if fun(item)]
@@ -37,116 +36,65 @@ def read_data(file_name, train_data_size, fun):
     train_data = data[:cut]
     test_data =  data[cut:]
     return train_data, test_data
-
-def process_probatility(data):
-    data_dict = defaultdict(list)
-    for v, k in data: data_dict[tuple(v)].append(k)
-    weighted_data = {}
-    for key, labels in data_dict.items():
-        weighed_labels = [0, 0, 0, 0, 0, 0, 0, 0]
-        for label in labels:
-            i = 0
-            for v in label:
-                if (v > 0):
-                    weighed_labels[i] += 1
-                    break
-                i += 1
-        count = len(labels)
-        for i in range(0,len(weighed_labels)):
-            weighed_labels[i] /= count
-        weighted_data[tuple(key)] = weighed_labels
-    return weighted_data
-
-def export_processed_data(data):
-    # save to file:
-    with open('Connect4_train_processed.json', 'w') as f:
-        for k, v in data:
-            line = '{}, {}'.format(k, v) 
-            print(line, file=f)     
-
+  
 def filterData(record):
     meta, _, _ = record
-    return meta[2] == 1 and meta[3] == 100 
+    return meta[2] == 0 and meta[3] == 100
 
-def guess_dumbly(data):
-    correct = [0,0,0,0,0,0,0,0]
-    for v, k in data:
-        for i in range(0, len(k)):
-            if (k[i] == 1):
-                correct[i] += 1
-    print(correct)
-    print(len(data))
-
-lastIndex = 0
-def read_from_csv(batch_size, rand):
-    global lastIndex
+def read_from_csv(batch_size):
     features = []
     labels = []
-    if rand:
-        for i in range(0, batch_size):
-            feature, label = random.choice(train_data)
-            features.append(feature)
-            labels.append(label)
-        return features, labels
 
-    trainLen = len(train_data)
-    if lastIndex + batch_size < trainLen:
-        end = lastIndex + batch_size
-        batch = train_data[lastIndex:end]
-        lastIndex = end
-    else:
-        rest = lastIndex + batch_size - trainLen
-        batch = train_data[lastIndex:] + train_data[:rest]
-        lastIndex = rest         
-        
-    for feature, label in batch:
+    for i in range(0, batch_size):
+        feature, label = random.choice(train_data)
         features.append(feature)
         labels.append(label)
-
     return features, labels
-
 
 # -------------------------------------------------------------------
 #                           Create Model
 # -------------------------------------------------------------------
 
+x = tf.placeholder(tf.float32, shape=[None, state_size], name='x')
+y_true = tf.placeholder(tf.float32, shape=[None, num_actions], name='y_true')
+y_true_cls = tf.argmax(y_true, dimension=1)
 
-
-def fully_connected_network(layers, randSample):
-    x = tf.placeholder(tf.float32, shape=[None, state_size], name='x')
-    y_true = tf.placeholder(tf.float32, shape=[None, num_actions], name='y_true')
-    y_true_cls = tf.argmax(y_true, dimension=1)
+def fully_connected_network(layers, name):
     x_pretty = pt.wrap(x)
     with pt.defaults_scope(activation_fn=tf.nn.relu):
         network = x_pretty
         i = 1
         for size in layers:
-            print(size)
-            result = network.fully_connected(size=size, name='layer_fc{0}'.format(i))
-            print(result)
+            network = network.fully_connected(size=size, name='{0}_layer_fc{1}_{2}'.format(name, i, size))
             i += 1
-        y_pred, loss = network.softmax_classifier(num_classes=num_actions, labels=y_true)
-        if randSample:
+        with tf.name_scope("{0}_softmax".format(name)):
+            y_pred, _ = network.softmax(labels=y_true)
             loss = tf.reduce_mean(tf.squared_difference(y_pred, y_true))
 
-        y_pred_cls = tf.argmax(y_pred, dimension=1)
-        correct_prediction = tf.equal(y_pred_cls, y_true_cls)
-        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-        optimizer = tf.train.AdamOptimizer().minimize(loss)
-        name = ', '.join(str(x) for x in layers) + (' Default_Loss' if randSample else '') 
-        return (name, optimizer, accuracy, ([],[]), (x, y_true), randSample)
+        with tf.name_scope("{0}_accuracy".format(name)):
+            y_pred_cls = tf.argmax(y_pred, dimension=1)
+            correct_prediction = tf.equal(y_pred_cls, y_true_cls)
+            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        
+        with tf.name_scope("{0}_train".format(name)):
+            optimizer = tf.train.AdamOptimizer(learning_rate=1e-5).minimize(loss)
+
+        name = ', '.join(str(x) for x in layers[:len(layers)-1]) 
+        return (name, optimizer, accuracy, ([],[]), (y_pred,loss))
 
 networks = [ 
-    fully_connected_network([], True)
+    fully_connected_network([128,64,128,64,128,8], "A"),
+    fully_connected_network([1000,250,500,250,1000,8], "B"),
+    fully_connected_network([128,64,128,8], "C"),
+    fully_connected_network([8], "D")
 ]
-
-
     
 # Splits the training and test data 80/20.
 train_data, test_data = read_data('data/connect4_big.csv', 0.8, filterData)
-#probatilities = process_probatility(train_data + test_data)
-#guess_dumbly(train_data+test_data)
 
+# -------------------------------------------------------------------
+#                           Train Model
+# -------------------------------------------------------------------
 
 session = tf.Session()
 session.run(tf.global_variables_initializer())
@@ -157,38 +105,32 @@ def optimize(num_iterations):
     # Start-time used for printing time-usage below.
     start_time = time.time()
 
+    feed_dict_test = {x: [i[0] for i in test_data] ,
+                      y_true: [i[1] for i in test_data] }
     for i in range(0, num_iterations):
-        x_batch, y_true_batch = read_from_csv(train_batch_size,True)
+        x_batch, y_true_batch = read_from_csv(train_batch_size)
 
-        for _, optimizer, _, _, (x,y_true), rndSample in networks:
-            feed_dict_train = {x: x_batch,
+        feed_dict_train = {x: x_batch,
                             y_true: y_true_batch}
+        for _, optimizer, _, _, rndSample in networks:
             session.run(optimizer, feed_dict=feed_dict_train)
-        
+
         if i % 10 == 0:
-            for name, _, accuracy, (plotx,ploty), (x, y_true), _ in networks:
-                feed_dict_test = {x: [i[0] for i in test_data] ,
-                                    y_true: [i[1] for i in test_data] }
+            for name, _, accuracy, (plotx,ploty), _ in networks:
                 acc = session.run(accuracy, feed_dict=feed_dict_test)
 
                 plotx.append(i)
                 ploty.append(acc)
 
-                #msg = "Optimization Iteration: {0:>6} Test Correct {1:>6.1%} Name {2}"
-                #print(msg.format(i + 1, acc, name))
+                msg = "Optimization Iteration: {0:>6} Test Correct {1:>6.1%} Name {2}"
+                print(msg.format(i + 1, acc, name))
 
     end_time = time.time()
     time_dif = end_time - start_time
     print("Time usage: " + str(timedelta(seconds=int(round(time_dif)))))
 
-def argmax_label(label):
-    maxi = 0
-    for i in range(0, len(label)):
-        if (label[i] > label[maxi]):
-            maxi = i
-    return maxi
+optimize(10000)
 
-optimize(5000)
 for (name, (x,y)) in [(i[0], i[3]) for i in networks]:
     plt.plot(x,y,label=name)
 plt.ylabel('Test Accuracy (%)')

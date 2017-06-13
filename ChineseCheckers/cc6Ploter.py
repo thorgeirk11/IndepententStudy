75d207c0906d45ade9bd8912c592fa54ed638e7a
Þorgeir <thorgeir@radiantgames.is>
2017-06-13 19:12:18 +0000

    The ploters now visulize through tensorboard

--- ChineseCheckers/cc6Ploter.py
+++ ChineseCheckers/cc6Ploter.py
@@ -1,10 +1,7 @@
-# %matplotlib inline
-
 import csv
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import numpy as np
-#from sklearn.metrics import confusion_matrix
 import time
 from datetime import timedelta
 import math
@@ -15,6 +12,9 @@ import numpy
 import prettytensor as pt
 import pandas as pd
 import random
+
+
+num_iterations = 50000
 
 metadata_size = 4
 state_size = 253
@@ -39,104 +39,103 @@ def read_data(file_name, train_data_size, fun):
   
 def filterData(record):
     meta, _, _ = record
-    return meta[2] == 0 and meta[3] > 0
+    return meta[2] == 0 and meta[3] == 100
 
 def read_from_csv(batch_size):
     features = []
     labels = []
-
     for i in range(0, batch_size):
         feature, label = random.choice(train_data)
         features.append(feature)
         labels.append(label)
     return features, labels
-
-# -------------------------------------------------------------------
-#                           Create Model
-# -------------------------------------------------------------------
-
-x = tf.placeholder(tf.float32, shape=[None, state_size], name='x')
-y_true = tf.placeholder(tf.float32, shape=[None, num_actions], name='y_true')
-y_true_cls = tf.argmax(y_true, dimension=1)
-
-def fully_connected_network(layers):
-    x_pretty = pt.wrap(x)
-    with pt.defaults_scope(activation_fn=tf.nn.relu):
-        network = x_pretty
-        i = 1
-        for size in layers:
-            #with tf.name_scope('fully_connected'):
-            network = network.fully_connected(size=size, name='layer_fc{0}_{1}'.format(i, size))
-            i += 1
-        with tf.name_scope("softmax"):
-            y_pred, _ = network.softmax_classifier(num_classes=num_actions, labels=y_true)
-            loss = tf.reduce_mean(tf.squared_difference(y_pred, y_true))
-
-        with tf.name_scope("accuracy"):
-            y_pred_cls = tf.argmax(y_pred, dimension=1)
-            correct_prediction = tf.equal(y_pred_cls, y_true_cls)
-            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-        
-        with tf.name_scope("train"):
-            optimizer = tf.train.AdamOptimizer(learning_rate=1e-5).minimize(loss)
-
-        name = ', '.join(str(x) for x in layers) 
-        return (name, optimizer, accuracy, ([],[]), (y_pred,loss))
-
-
-networks = [ 
-    fully_connected_network([]),
-]
-
-
     
 # Splits the training and test data 80/20.
 train_data, test_data = read_data('data/chinese_checkers_6_role0_noNoop.csv', 0.8, filterData)
 
 # -------------------------------------------------------------------
-#                           Train Model
+#                           Create Model
 # -------------------------------------------------------------------
 
-session = tf.Session()
-session.run(tf.global_variables_initializer())
 
-train_batch_size = 64
+def BuildAndTrain(layers, learning_rate):
+    tf.reset_default_graph()
+    session = tf.Session()
 
-def optimize(num_iterations):
+    x = tf.placeholder(tf.float32, shape=[None, state_size], name='x')
+    y_true = tf.placeholder(tf.float32, shape=[None, num_actions], name='y_true')
+    y_true_cls = tf.argmax(y_true, dimension=1)
+
+    hparam = '_'.join(str(x) for x in layers[:len(layers)]) + '_LR_' + str(learning_rate) 
+   
+    # -------------------------------------------------------------------
+    #                           Pretty Network
+    # -------------------------------------------------------------------
+    x_pretty = pt.wrap(x)
+    with pt.defaults_scope(activation_fn=tf.nn.relu):
+        network = x_pretty
+        i = 1
+        for size in layers:
+            network = network.fully_connected(size=size, name='layer_fc{0}_{1}'.format(i, size))
+            i += 1
+
+        with tf.name_scope("loss"):     
+            network = network.fully_connected(size=num_actions)
+            y_pred, _ = network.softmax(labels=y_true)
+            loss = tf.reduce_mean(tf.squared_difference(y_pred, y_true))
+            tf.summary.scalar("loss", loss)
+
+        with tf.name_scope("accuracy"):
+            y_pred_cls = tf.argmax(y_pred, dimension=1)
+            correct_prediction = tf.equal(y_pred_cls, y_true_cls)
+            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+            tf.summary.scalar("accuracy", accuracy)
+
+        with tf.name_scope("train"):
+            optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
+
+    
+    # -------------------------------------------------------------------
+    #                           Train Model
+    # -------------------------------------------------------------------
+
+    train_batch_size = 64
+
+    session.run(tf.global_variables_initializer())
+
+    merged_summary = tf.summary.merge_all()
+    file_writer = tf.summary.FileWriter('/logs/cc6/cmp3/' + hparam)
+    file_writer.add_graph(session.graph)
+    
     # Start-time used for printing time-usage below.
     start_time = time.time()
 
     feed_dict_test = {x: [i[0] for i in test_data] ,
-                      y_true: [i[1] for i in test_data] }
+                    y_true: [i[1] for i in test_data] }
     for i in range(0, num_iterations):
         x_batch, y_true_batch = read_from_csv(train_batch_size)
 
         feed_dict_train = {x: x_batch,
                             y_true: y_true_batch}
-        for _, optimizer, _, _, rndSample in networks:
-            session.run(optimizer, feed_dict=feed_dict_train_rnd)
-
+        session.run(optimizer, feed_dict=feed_dict_train)
         if i % 10 == 0:
-            for name, _, accuracy, (plotx,ploty), _ in networks:
-                acc = session.run(accuracy, feed_dict=feed_dict_test)
-
-                plotx.append(i)
-                ploty.append(acc)
-
-                msg = "Optimization Iteration: {0:>6} Test Correct {1:>6.1%} Name {2}"
-                print(msg.format(i + 1, acc, name))
-
+            s = session.run(merged_summary, feed_dict=feed_dict_test)
+            file_writer.add_summary(s,i)
     end_time = time.time()
     time_dif = end_time - start_time
     print("Time usage: " + str(timedelta(seconds=int(round(time_dif)))))
 
-optimize(2000)
-
-for (name, (x,y)) in [(i[0], i[3]) for i in networks]:
-    plt.plot(x,y,label=name)
-plt.ylabel('Test Accuracy (%)')
-plt.xlabel('Optimization Iteration')
-plt.title("Plot")
-plt.legend()
-plt.savefig('Plot.png')
-plt.show()
+networks = [ 
+    BuildAndTrain([], learning_rate=1e-4),
+    BuildAndTrain([100], learning_rate=1e-4),
+    BuildAndTrain([100,50,25,50,25,50,100], learning_rate=1e-4),
+    BuildAndTrain([128,64,128], learning_rate=1e-3),
+    BuildAndTrain([128,64,128,64,128], learning_rate=1e-4),
+    BuildAndTrain([128,64,128,64,128], learning_rate=1e-3),
+    BuildAndTrain([128,64,128], learning_rate=1e-4),
+    BuildAndTrain([128,64,128], learning_rate=1e-3),
+    BuildAndTrain([1000,250,500,250,1000], learning_rate=1e-4),
+    BuildAndTrain([1000,250,500,250,1000], learning_rate=1e-3),
+    BuildAndTrain([2000,500,2000], learning_rate=1e-4),
+    BuildAndTrain([2000,500,2000], learning_rate=1e-3),
+]
--- Connect4/connect4.py
+++ Connect4/connect4.py
@@ -20,16 +20,14 @@ metadata_size = 4
 state_size = 135
 num_actions = 8
 
-
 # -------------------------------------------------------------------
 #                           Read Data
 # -------------------------------------------------------------------
 
 def read_data(file_name, train_data_size, fun):
-    # Match.connect4.1240881643605, 1, 1, 100
     meta = pd.read_csv(file_name, usecols = range(0,metadata_size), header = None)
     features = pd.read_csv(file_name, usecols = range(metadata_size,metadata_size+state_size), header = None)
-    labels  = pd.read_csv(file_name, usecols = range(metadata_size +state_size, metadata_size + state_size + num_actions), header = None)
+    labels  = pd.read_csv(file_name, usecols = range(metadata_size + state_size, metadata_size + state_size + num_actions), header = None)
     
     data = list(zip(meta.values, features.values, labels.values))
     data = [(item[1],item[2]) for item in data if fun(item)]
@@ -38,11 +36,10 @@ def read_data(file_name, train_data_size, fun):
     train_data = data[:cut]
     test_data =  data[cut:]
     return train_data, test_data
-
+  
 def filterData(record):
-    meta, key, label = record
-    # Only matches where role 1 won
-    return meta[2] == 1 and meta[3] == 100
+    meta, _, _ = record
+    return meta[2] == 0 and meta[3] == 100
 
 def read_from_csv(batch_size):
     features = []
@@ -53,7 +50,6 @@ def read_from_csv(batch_size):
         labels.append(label)
     return features, labels
 
-
 # -------------------------------------------------------------------
 #                           Create Model
 # -------------------------------------------------------------------
@@ -62,43 +58,45 @@ x = tf.placeholder(tf.float32, shape=[None, state_size], name='x')
 y_true = tf.placeholder(tf.float32, shape=[None, num_actions], name='y_true')
 y_true_cls = tf.argmax(y_true, dimension=1)
 
-def fully_connected_network(layers):
+def fully_connected_network(layers, name):
     x_pretty = pt.wrap(x)
     with pt.defaults_scope(activation_fn=tf.nn.relu):
         network = x_pretty
         i = 1
         for size in layers:
-            #with tf.name_scope('fully_connected'):
-            network = network.fully_connected(size=size, name='layer_fc{0}'.format(i))
+            network = network.fully_connected(size=size, name='{0}_layer_fc{1}_{2}'.format(name, i, size))
             i += 1
-        with tf.name_scope("softmax"):
+        with tf.name_scope("{0}_softmax".format(name)):
             y_pred, _ = network.softmax(labels=y_true)
             loss = tf.reduce_mean(tf.squared_difference(y_pred, y_true))
+            tf.summary.scalar("{0}_softmax".format(name), loss)
 
-        with tf.name_scope("accuracy"):
+        with tf.name_scope("{0}_accuracy".format(name)):
             y_pred_cls = tf.argmax(y_pred, dimension=1)
             correct_prediction = tf.equal(y_pred_cls, y_true_cls)
             accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-        
-        with tf.name_scope("train"):
-            optimizer = tf.train.AdamOptimizer().minimize(loss)
-        
-        name = ', '.join(str(x) for x in layers) 
-        return (name, optimizer, accuracy, ([],[]))
+            tf.summary.scalar("{0}_accuracy".format(name), accuracy)
 
-networks = [ 
-    fully_connected_network([200,100,200,100,200,8])
-]
+        with tf.name_scope("{0}_train".format(name)):
+            optimizer = tf.train.AdamOptimizer(learning_rate=1e-5).minimize(loss)
 
+        name = ', '.join(str(x) for x in layers[:len(layers)-1]) 
+        return (name, optimizer, accuracy)
+
+network = fully_connected_network([128,64,128,64,128,8], "A")
     
 # Splits the training and test data 80/20.
 train_data, test_data = read_data('data/connect4_big.csv', 0.8, filterData)
-#probatilities = process_probatility(train_data + test_data)
-#guess_dumbly(train_data+test_data)
+
+# -------------------------------------------------------------------
+#                           Train Model
+# -------------------------------------------------------------------
 
 session = tf.Session()
 session.run(tf.global_variables_initializer())
-file_writer = tf.summary.FileWriter('/logs/4')
+
+merged_summary = tf.summary.merge_all()
+file_writer = tf.summary.FileWriter('/logs/5')
 file_writer.add_graph(session.graph)
 
 train_batch_size = 64
@@ -117,20 +115,20 @@ def optimize(num_iterations):
 
         feed_dict_train = {x: x_batch,
                            y_true: y_true_batch}
-        for _, optimizer, _, _ in networks:
-            session.run(optimizer, feed_dict=feed_dict_train)
+        _, optimizer, _ = network
+        session.run(optimizer, feed_dict=feed_dict_train)
 
-        if i % 50 == 0:
-            for name, _, accuracy, (plotx,ploty) in networks:
-                acc = session.run(accuracy, feed_dict=feed_dict_test)
-                if (acc > best):
-                    best = acc
-                    since_last_best = 0
-                plotx.append(i)
-                ploty.append(acc)
+        if i % 5 == 0:
+            #for name, _, accuracy in networks:
+            s = session.run(merged_summary, feed_dict=feed_dict_test)
+            file_writer.add_summary(s,i)
+                #acc = session.run(accuracy, feed_dict=feed_dict_test)
+                #if (acc > best):
+                #    best = acc
+                #    since_last_best = 0
 
-                msg = "Optimization Iteration: {0:>6} Test Correct {1:>6.1%} Best {2}"
-                print(msg.format(i + 1, acc, best))
+                #msg = "Optimization Iteration: {0:>6} Test Correct {1:>6.1%} Best {2}"
+                #print(msg.format(i + 1, acc, best))
             
         since_last_best += 1
         if since_last_best > num_iterations:

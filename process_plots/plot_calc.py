712c12c0a1830ff77ddee497a96b8cea0ef3c81f
Þorgeir <thorgeir@radiantgames.is>
2017-07-29 10:02:47 +0000

    Updated the plot extraction from experiment runs

--- process_plots/plot_calc.py
+++ process_plots/plot_calc.py
@@ -5,7 +5,7 @@ import scipy.stats
 import urllib
 from collections import OrderedDict
 
-def mean_confidence_interval(data, confidence=0.95):
+def mean_confidence_interval(data, confidence=0.99):
     a = 1.0*np.array(data)
     n = len(a)
     mean, se = np.mean(a), scipy.stats.sem(a)
@@ -13,7 +13,7 @@ def mean_confidence_interval(data, confidence=0.95):
     return mean, interval
 
 
-FILE_COUNT = 105
+FILE_COUNT = 150
 TENSORBOARD_URL = "http://localhost:6006/data/scalars?run=C%5C{0}%5C{2}%5Crole_0{1}&tag=val_accuracy&format=csv"
 
 games = ["connect4","chinese_checkers_6","breakthrough"]
@@ -31,20 +31,20 @@ for game in games:
             meanArr = []
             for x,y in csv.values:
                 meanArr.append(y)
-                if r % 20 == 10:
+                if r % 3 == 1:
                     mean = np.mean(meanArr)
-                    #print meanArr
-                    #print mean
-                    if x - 10 in run_data.keys():
-                        run_data[x - 10].append(mean)
+                    if x - 30 in run_data.keys():
+                        run_data[x - 30].append(mean)
                     else:
-                        run_data[x - 10] = [mean]
+                        run_data[x - 30] = [mean]
                     meanArr = []
                 r += 1
         interval_data = []
         for key, val_arr in run_data.items():
-            mean, interval = mean_confidence_interval(val_arr)
-            interval_data.append([key, mean, interval])
+            std = np.std(val_arr)
+            mean, interval_95 = mean_confidence_interval(val_arr, 0.95)
+            _, interval_99 = mean_confidence_interval(val_arr, 0.99)
+            interval_data.append([key, mean, std, mean + interval_95, mean + interval_99])
         filename = game + run_type + "_interval_data.csv"
         interval_data = np.array(interval_data)
         np.savetxt(filename, interval_data, delimiter=",")
